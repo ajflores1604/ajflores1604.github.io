b220f7f4ff4e0e0cf27f7baf6bfde0ed35c5180f
Alexander Jose Flores <ajflores1604@gmail.com>
2019-12-27 18:54:56 -0600

    initial three scene

--- script.js
+++ script.js
@@ -1,69 +1,76 @@
 
-function handleMotionEvent(event) {
-    // console.log('fired motion event')
-//     acousticGuitar.play();
-    let pan = (event.accelerationIncludingGravity.x * -0.2);
-    let x = Math.abs(event.accelerationIncludingGravity.x * 30);
-    let y = Math.abs(event.accelerationIncludingGravity.y * .15);
-    // let z = Math.abs(event.accelerationIncludingGravity.z *.09);
-    // let z = event.accelerationIncludingGravity.z.toFixed(2);
+// function handleMotionEvent(event) {
+//     let pan = (event.accelerationIncludingGravity.x * -0.2);
+//     let x = Math.abs(event.accelerationIncludingGravity.x * 30);
+//     let y = Math.abs(event.accelerationIncludingGravity.y * .15);
+//     // let z = Math.abs(event.accelerationIncludingGravity.z *.09);
+//     // let z = event.accelerationIncludingGravity.z.toFixed(2);
 
-    x = x.toFixed(0)
-    // y = y.toFixed(0)
-    // z = z.toFixed(0)
-    TweenMax.to('#text', 1, { color: `hsl(${x},100%,50%)`});
-    TweenMax.to('#hed', 1, { opacity : `${y}`});
-    TweenMax.to(stereoPanner, 1, { pan : `${pan}`});
-    // stereoPanner.pan = pan;
-    //el.style.background = `hsl(${x},100%,50%)`;
-    // console.log(x, y, z)
-    // if (x > 200) {
-    //     el.innerHTML = 'Direction Change Works!!'
-    // } else {
-    //     el.innerHTML = 'Tween Max Includes'
-    // }
-
-}
-// let el = document.getElementById('text');
-
-// function handleLightEvent(event){
-//     console.log('something happened')
-//     console.log(event.value)
-//     if 
-//     el.style.background = `blue`;
+//     x = x.toFixed(0)
+//     // y = y.toFixed(0)
+//     // z = z.toFixed(0)
+//     TweenMax.to('#text', 1, { color: `hsl(${x},100%,50%)`});
+//     TweenMax.to('#hed', 1, { opacity : `${y}`});
+//     TweenMax.to(stereoPanner, 1, { pan : `${pan}`});
 // }
-function onClick() {
-    // feature detect
-    Pizzicato.context.resume();
-    if (typeof DeviceMotionEvent.requestPermission === 'function') {
-      DeviceMotionEvent.requestPermission()
-        .then(permissionState => {
-          if (permissionState === 'granted') {
-            window.addEventListener('devicemotion', handleMotionEvent, true);
-          }
-        })
-        .catch(console.error);
-    } else {
-      // handle regular non iOS 13+ devices
-        window.addEventListener('devicemotion', handleMotionEvent, true);
-        console.log('cry to your fruit overlords');
-    }
-  }// window.addEventListener('devicemotion', handleMotionEvent, true);
-// window.addEventListener('devicelight', handleLightEvent);
+
+// function onClick() {
+//     // feature detect
+//     Pizzicato.context.resume();
+//     if (typeof DeviceMotionEvent.requestPermission === 'function') {
+//       DeviceMotionEvent.requestPermission()
+//         .then(permissionState => {
+//           if (permissionState === 'granted') {
+//             window.addEventListener('devicemotion', handleMotionEvent, true);
+//           }
+//         })
+//         .catch(console.error);
+//     } else {
+//       // handle regular non iOS 13+ devices
+//         window.addEventListener('devicemotion', handleMotionEvent, true);
+//         console.log('cry to your fruit overlords');
+//     }
+//   }
 
 
-var stereoPanner = new Pizzicato.Effects.StereoPanner({
-    pan: 0.0
-});
-function playMusic(){
-    Pizzicato.context.resume();
-    var acousticGuitar = new Pizzicato.Sound('irene.mp3', function () {
-    // Sound loaded!
-    acousticGuitar.addEffect(stereoPanner);
-    acousticGuitar.play();
-});
-    
-}
+// var stereoPanner = new Pizzicato.Effects.StereoPanner({
+//     pan: 0.0
+// });
+// function playMusic(){
+//     Pizzicato.context.resume();
+//     var acousticGuitar = new Pizzicato.Sound('irene.mp3', function () {
+//     // Sound loaded!
+//     acousticGuitar.addEffect(stereoPanner);
+//     acousticGuitar.play();
+// });
+// }
 
-document.getElementById("butt").addEventListener("click",onClick);
-document.getElementById("butt").addEventListener("click",playMusic);
+// document.getElementById("butt").addEventListener("click",onClick);
+// document.getElementById("butt").addEventListener("click",playMusic);
+/////////////////////////////////////////////////////////////////////////////////////////
+
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+var animate = function () {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+};
+
+animate();
